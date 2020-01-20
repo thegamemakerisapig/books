438abee6e21c1e04bded7f15119e32061e62f23d
gamemaker <417824246@qq.com>
2020-01-20 10:10:31 +0800

    extract github config from code to properties

--- readme.md
+++ readme.md
@@ -1,8 +1,10 @@
 ##社区
 
 ##资料
-https://spring.io/guides
-https://spring.io/guides/gs/serving-web-content/
-
+[Spring文档](https://spring.io/guides)
+[Spring web文档](https://spring.io/guides/gs/serving-web-content/)
+[Bookstarp](https://www.bootcss.com/)
+[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
 ##工具
-https://git-scm.com/
+[Git](https://git-scm.com/)
+[Visual Paradigm]()
---
+++ src/main/java/work/books/books/controller/AuthorizeController.java
@@ -0,0 +1,40 @@
+package work.books.books.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import work.books.books.dto.AccessTokenDTO;
+import work.books.books.dto.GithubUser;
+import work.books.books.provider.GithubProvider;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String secret;
+
+    @Value("${github.redirect.uri}")
+    private  String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state")String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(secret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
